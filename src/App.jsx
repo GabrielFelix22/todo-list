510852e9047c5296253b98c9b53feca181b0ee54
Gabriel Felix <gabriel.alves2208@gmail.com>
2025-03-27 17:17:23 -0300

    Primeiro commit, projeto já concluido.

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import {
+  Button,
+  Check,
+  Container,
+  Input,
+  ListItem,
+  TodoList,
+  Trash,
+} from "./styles";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [inputTask, setInputTask] = useState([]);
+
+  function inputMudou(event) {
+    setInputTask(event.target.value);
+  }
+
+  function cliqueiNoBotao() {
+    if (inputTask) {
+      setList([...list, { id: uuid(), task: inputTask, checked: false }]);
+    }
+  }
+
+  function finalizarTarefa(id) {
+    const newList = list.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+
+    setList(newList);
+  }
+
+  function deletarItem(id) {
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
+  }
+
+  return (
+    <Container>
+      <TodoList>
+        <Input onChange={inputMudou} placeholder="O que tenha para fazer..." />
+        <Button onClick={cliqueiNoBotao}>Adicionar</Button>
+
+        <ul>
+          {list.length > 0 ? (
+            list.map((item) => (
+              <ListItem $isChecked={item.checked} key={item.id}>
+                <Check onClick={() => finalizarTarefa(item.id)} />
+                <li>{item.task}</li>
+                <Trash onClick={() => deletarItem(item.id)} />
+              </ListItem>
+            ))
+          ) : (
+            <h3>Nenhuma tarefa cadastrada</h3>
+          )}
+        </ul>
+      </TodoList>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,79 @@
+import { FcCheckmark, FcEmptyTrash } from 'react-icons/fc';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TodoList = styled.div`
+  background: #fff;
+  padding: 1.875rem 1.25rem;  // 30px 20px
+  border-radius: 0.625rem;  // 10px
+
+  ul {
+    padding: 0;
+    margin-top: 3.75rem;  // 60px
+  }
+`;
+
+export const Input = styled.input`
+  border-radius: 0.3125rem;  // 5px
+  border: 1px solid rgba(209, 211, 212, 0.4);
+  height: 2.5rem;
+  margin-right: 1.875rem;  // 30px
+  width: 21.375rem;  // 343px
+  padding: 0.625rem;  // 10px
+  font-size: 1rem;  // 16px
+`;
+
+export const Button = styled.button`
+  background: #007bff;
+  color: #fff;
+  border-radius: 0.3125rem;  // 5px
+  border: none;
+  height: 2.5rem;  // 40px
+  cursor: pointer;
+  font-size: 1.0625rem;  // 17px
+  line-height: 0.125rem;
+  width: 8.125rem;  // 130px
+
+  &:hover {
+    background:rgb(76, 157, 243);
+  }
+
+  &:active {
+    background: #0056b3;
+  }
+`;
+
+export const ListItem = styled.div`
+  background: ${(props) => (props.$isChecked ? '#E8FF8B' : '#E4E4E4')};
+  border-radius: 0.3125rem;  // 5px
+  box-shadow: 0.063rem 0.25rem 0.625rem rgba(0, 0, 0, 0.2);  // 1px 4px 10px
+  height: 3.75rem;  // 60px
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1.25rem;  // 20px
+  padding: 0.625rem;  // 10px
+  width: 31.25rem;  // 500px
+
+  li {
+    list-style: none;
+  }
+`;
+
+export const Trash = styled(FcEmptyTrash)`
+  cursor: pointer;
+  font-size: 1.25rem;  //20px
+`;
+
+export const Check = styled(FcCheckmark)`
+  cursor: pointer;
+  font-size: 1.25rem;  //20px
+`;
